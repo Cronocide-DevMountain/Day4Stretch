72fe453f39de3159c43ebb2ab2d6c4716fcda107
Daniel Dayley <danieldayley@aim.com>
2015-04-30 09:36:18 -0600

    Initial Commit

---
+++ Day4Stretch(Properties)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Day4Stretch(Properties)
+//
+//  Created by Daniel Dayley on 4/30/15.
+//  Copyright (c) 2015 Daniel Dayley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
